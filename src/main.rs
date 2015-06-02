7656c565a79875c708f77e75a970ca791abae896
Dan Fockler <dfockler13@gmail.com>
2015-06-01 19:22:06 -0700

    figuring out how modules work :)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+mod parser;
 
 fn main() {
     let filename = env::args().nth(1).expect("You need an input file");
@@ -10,6 +11,10 @@ fn main() {
     let mut contents = String::new();
 
     file.read_to_string(&mut contents).unwrap();
+    
+    let lines: Vec<&str> = contents.split("\n").collect();
 
-    println!("{:?}", contents); 
+    for line in lines {
+        parser::parse_line(line);
+    }
 }
